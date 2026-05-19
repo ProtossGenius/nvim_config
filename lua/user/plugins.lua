d533cc89f3b6551c216a58bef00adb672a463858
suremoon <suremoon@suremoondeMacBook-Pro.local>
2026-05-19 10:56:56 +0800

    add window jump, remove {} binding.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -348,14 +348,7 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require('aerial').setup({
-        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-        on_attach = function(bufnr)
-          -- Jump forwards/backwards with '{' and '}'
-          vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
-          vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
-        end
-      })
+      require('aerial').setup({})
     end
   },
 
